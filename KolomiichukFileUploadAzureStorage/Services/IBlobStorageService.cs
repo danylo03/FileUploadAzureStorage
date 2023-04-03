3c7f75acffe680cda393c2df2ffd908cb834fc41
danylo03 <128912512+danylo03@users.noreply.github.com>
2023-04-03 13:33:04 +0300

    Complited!

---
+++ KolomiichukFileUploadAzureStorage/Services/IBlobStorageService.cs
@@ -0,0 +1,8 @@
+﻿namespace KolomiichukFileUploadAzureStorage.Services
+{
+    public interface IBlobStorageService
+    {
+        Task<string> UploadFileToBlobAsync(string strFileName, string contecntType, Stream fileStream);
+        Task<bool> DeleteFileToBlobAsync(string strFileName);
+    }
+}
